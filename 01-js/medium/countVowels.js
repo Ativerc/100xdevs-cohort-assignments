c139575ed736a0e7857abebec3fbd4047a67dfa2
Pratyush Mohapatra <Ativerc@users.noreply.github.com>
2023-12-13 21:54:27 +0530

    feat: Week1 > Medium > countVowels.js complete!

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -6,7 +6,14 @@
 */
 
 function countVowels(str) {
-    // Your code here
+    let vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+    let vowelCount = 0
+    for (let i = 0; i < str.length; i++) {
+      if (vowels.includes(str[i])) {
+        vowelCount += 1;
+      }
+    }
+    return vowelCount;
 }
 
 module.exports = countVowels;
