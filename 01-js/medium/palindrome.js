96e5d013a2b806da50d0dd77a8f90832ab860d09
Pratyush Mohapatra <Ativerc@users.noreply.github.com>
2023-12-13 22:48:54 +0530

    feat: Week 1 > Medium > palindrome.js complete!

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -4,7 +4,19 @@
 */
 
 function isPalindrome(str) {
+  str = str.toLowerCase().replace(/[.,\/#?!$%\^&\*;:{}=\-_`~()]/g,"").replace(/\s+/g, "");;
+  let strArray = str.split("");
+  let reversedStrArray = strArray.slice().reverse();
+  console.log(str);
+  let count = 0;
+  for (let i = 0; i < reversedStrArray.length; i++) {
+    if (reversedStrArray[i] != strArray[i]) {
+      return false;
+    }
+  }
   return true;
 }
+
+// console.log(isPalindrome('tonnot'));
 
 module.exports = isPalindrome;
