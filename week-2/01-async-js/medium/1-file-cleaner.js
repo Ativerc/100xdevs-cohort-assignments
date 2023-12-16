6b3f63bdd1a88e24f97d18455ff5b22561ed3820
ativerc <Ativerc@users.noreply.github.com>
2023-12-16 18:54:30 +0530

    feat: Week 2 > Async > Medium > 1-file-cleaner.js done!

--- week-2/01-async-js/medium/1-file-cleaner.js
+++ week-2/01-async-js/medium/1-file-cleaner.js
@@ -13,3 +13,23 @@ After the program runs, the output should be
 hello world my name is raman
 ```
 */
+
+let fs = require('fs');
+
+fs.readFile("file-cleaner.txt", "utf-8", function(err, data) {
+  if (err){
+    console.log(err);
+  } else {
+    const str = data;
+    const words = str.split(/\s+/);
+    const sentence = words.join(" ");
+    fs.writeFile("file-cleaner.txt", sentence,"utf-8", function(err) {
+      if (err) {
+        console.log("File write errors!:");
+        console.log(err);
+      } else {
+        console.log("File written successfully!")
+        console.log(sentence)
+      }
+  })
+}})
