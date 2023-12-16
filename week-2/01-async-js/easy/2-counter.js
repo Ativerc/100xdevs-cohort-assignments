e048b90940f139e0ecb3cf5a41cb3e9d72129a86
ativerc <Ativerc@users.noreply.github.com>
2023-12-16 17:27:33 +0530

    feat: Week 2 > Async > Easy > 2-counter.js

--- week-2/01-async-js/easy/2-counter.js
+++ week-2/01-async-js/easy/2-counter.js
@@ -1,9 +1,17 @@
-## Counter without setInterval
+/* ## Counter without setInterval
 
 Without using setInterval, try to code a counter in Javascript. There is a hint at the bottom of the file if you get stuck.
+*/
 
+let timerValue = 0
 
+function counter() {
+  console.log(timerValue);
+  timerValue++;
+  setTimeout(counter, 1000);
+}
 
+counter()
 
 
 
@@ -67,10 +75,4 @@ Without using setInterval, try to code a counter in Javascript. There is a hint 
 
 
 
-
-
-
-
-
-
-(Hint: setTimeout)
+// (Hint: setTimeout)
