fe4f1776081df0d8a360596892f6c20722c2cdf4
ativerc <Ativerc@users.noreply.github.com>
2023-12-17 15:12:27 +0530

    feat: Week 2 > Async > Medium > 2-clock.js complete!

--- week-2/01-async-js/medium/2-clock.js
+++ week-2/01-async-js/medium/2-clock.js
@@ -1,3 +1,4 @@
+/*
 Using `1-counter.md` or `2-counter.md` from the easy section, can you create a
 clock that shows you the current machine time?
 
@@ -6,3 +7,34 @@ Can you make it so that it updates every second, and shows time in the following
  - HH:MM::SS (Eg. 13:45:23)
 
  - HH:MM::SS AM/PM (Eg 01:45:23 PM)
+*/
+
+function zeroPadding(timeValue) {
+  timeValue = timeValue < 10 ? '0'+timeValue : timeValue;
+  return timeValue;
+}
+
+function getTimeString(date, type) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+  let ampm = hours >= 12 ? 'PM' : 'AM';
+  // Hour Correction
+  let twelveHours = hours % 12; // For Hours above 12
+  twelveHours = twelveHours ? twelveHours : 12; // For '0' Hour 
+  // Hours, Minutes, Second Zero Padding
+  
+
+  if (type == 12) {
+    let twelveHourString = `${zeroPadding(twelveHours)}:${zeroPadding(minutes)}:${zeroPadding(seconds)} ${ampm}`;
+    return twelveHourString;
+  }
+  return `${zeroPadding(hours)}:${zeroPadding(minutes)}:${zeroPadding(seconds)}`
+}
+
+function timer() {
+  let d = new Date();
+  console.log(`24Hour Time - ${getTimeString(d)} 12Hour Time - ${getTimeString(d, 12)}`);
+}
+
+setInterval(timer, 1000);
