7a589b64f96fb4f17af270a740ee97546da1cbcc
ativerc <Ativerc@users.noreply.github.com>
2024-02-05 21:31:44 +0530

    feat: Week 4 HardTodo > Working on Mark As Complete Button

--- week-4/react-basics/hard-todo-using-useState/src/App.jsx
+++ week-4/react-basics/hard-todo-using-useState/src/App.jsx
@@ -3,29 +3,31 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-let id = 1;
+let id = 0;
 function App() {
   const [todos, setTodos] = useState([{
       id: id,
-      title: "Title of Todo 1",
-      description: "Todo 1 Description",
+      title: `Title of Todo ${id}`,
+      description: `Todo ${id} Description`,
       status: true
     }])
 
   function addTodo() {
+    ++id;
     const todoTitle = document.querySelector('#customTodoTitle');
     const todoDescription = document.querySelector('#customTodoDescription');
 
-    const todoTitleText = (!todoTitle.value) ? "Random Todo Title" : todoTitle.value;
-    const todoDescriptionText = (!todoDescription.value) ? "Random Todo Description" : todoDescription.value;
+    const todoTitleText = (!todoTitle.value) ? `Random Todo Title ${id}` : todoTitle.value;
+    const todoDescriptionText = (!todoDescription.value) ? `Random Todo Description ${id}` : todoDescription.value;
 
     setTodos(todos.concat({
-      id: ++id,
+      id: id,
       title: todoTitleText,
       description: todoDescriptionText,
       status: false,
     }))
     }
+  
   
   return (
     <div>
@@ -38,19 +40,25 @@ function App() {
       </div>
       <h1>Todo List</h1>
       {todos.map(todoItem => {
-        return <Todo key={todoItem.id} title={todoItem.title} description={todoItem.description} status={todoItem.status}></Todo>
+        return <Todo key={todoItem.id} id={todoItem.id} title={todoItem.title} description={todoItem.description} status={todoItem.status}></Todo>
       })}
     </div>
   )
 }
 
 function Todo(props) {
+  function changeStatus(e) {
+    const targetId = e.target.parentElement.id;
+    // console.log(todos[targetId])
+    // const targetStatus = 
+    console.log(`Change Status ${e.target.parentElement.id}`)
+  }
   return (
-    <div>
+    <div id={props.id}>
       <h2>{props.title}</h2>
       <p>{props.description}</p>
       <p>Status: {props.completed ? "Complete" : "Incomplete"}</p>
-      <button>Mark As Complete</button>
+      <button onClick={(e) => {changeStatus(e)}}>Mark As Complete</button>
     </div>
   )
 }
