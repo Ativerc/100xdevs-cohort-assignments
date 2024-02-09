295874f8c7dfc043acf2931540ff41905ba4e32c
ativerc <Ativerc@users.noreply.github.com>
2024-02-09 16:23:21 +0530

    feat: Week 3 > Mongo+JWT Auth > DB Schema done

--- week-3/04-mongo-with-jwt-auth/db/index.js
+++ week-3/04-mongo-with-jwt-auth/db/index.js
@@ -1,19 +1,34 @@
 const mongoose = require('mongoose');
+const DB_CONNECTION_STRING = process.env.db_connection_string
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect(DB_CONNECTION_STRING);
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
+    username: String,
+    password: String
 });
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
+    username: String,
+    password: String,
+    courses: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course'
+    }]
 });
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
+    id: Number,
+    title: String,
+    description: String,
+    price: Number,
+    imageLink: String,
+    published: Boolean
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
