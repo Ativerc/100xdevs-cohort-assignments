381dbf8c2057927039cae124db8ded43722b1202
ativerc <Ativerc@users.noreply.github.com>
2024-02-13 19:06:40 +0530

    feat: Week 3 > Mongo+JWT > Router POST /user/signup done!

--- week-3/04-mongo-with-jwt-auth/routes/user.js
+++ week-3/04-mongo-with-jwt-auth/routes/user.js
@@ -1,14 +1,31 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
+const { User, Course } = require("../db/index.js")
 
 // User Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     // Implement user signup logic
+    /*
+    * Get username and password
+    * Does this username already exist in the database? -> ERR
+    * Doesn't exist? Save username and password and send bacj SUCCESS message
+    */ 
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const userExists = await User.findOne({username}).exec();
+    if (userExists) {
+        res.status(400).json({message: "User Already Exists!"});
+    } else {
+        await User.create({username, password});
+        res.status(200).json({message: 'User created successfully'});
+    }
 });
 
 router.post('/signin', (req, res) => {
     // Implement admin signup logic
+    
 });
 
 router.get('/courses', (req, res) => {
