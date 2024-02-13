f8e1bd052e5b35405ad2d0cb943f5a4259eb2d7a
ativerc <Ativerc@users.noreply.github.com>
2024-02-13 22:57:46 +0530

    feat: Week 3 > 4-Mongo+JWT > User Middleware done!

--- week-3/04-mongo-with-jwt-auth/middleware/user.js
+++ week-3/04-mongo-with-jwt-auth/middleware/user.js
@@ -1,6 +1,24 @@
+const jwt = require('jsonwebtoken');
+const jwtPassword = process.env.jwtSecret;
+
 function userMiddleware(req, res, next) {
-    // Implement user auth logic
-    // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+    try{
+        const receivedToken = req.headers.authorization;
+        const tokenArray = receivedToken.split(" ");
+        const userJwt = tokenArray[1];
+
+        const jwtVerifyOutput = jwt.verify(userJwt, jwtPassword);
+        if (jwtVerifyOutput.username) {
+            next();
+        } else {
+            res.status(403).json({
+                msg: "You are not authenticated"
+            })
+        }
+    } catch(error) {
+        console.error(error.message)
+        console.error(error);
+    }
 }
 
 module.exports = userMiddleware;
